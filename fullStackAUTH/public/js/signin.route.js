3737c5f121994c2bf85837f8cfd13992c6c6b90b
omarkhatib <omarkh1995@yahoo.com>
2017-05-22 17:48:30 +0300

    make red border around username and password if there is an error (not registered user or not correct password) and in signup username if username is token

--- fullStackAUTH/public/js/signin.route.js
+++ fullStackAUTH/public/js/signin.route.js
@@ -13,17 +13,30 @@ app.controller("signinCtrl", function($scope, authService, $location, tokenServi
     authService.postSignin($scope.userinput).then(function(response) {
       tokenService.setToken(response.data.token);
       privService.setUser(response.data.user);
-      $location.path("/home");
+      $location.path("/feeds");
     }, function(response) {
       console.log(response.status);
+        if(response.status==404){
+            $('#signinUsername').css('border','solid red 1px');
+            
+        }
+        else if(response.status==403){
+            $('#signinPassword').css('border','solid red 1px');
+            
+        }
     });
   }
         $scope.userInput = {};
   $scope.signup = function() {
     authService.postSignup($scope.userInput).then(function(response) {
       alert("You have signed up for an account , please signin");
+        $scope.userInput = {};
+        $scope.confirmPassword = "";
     }, function(response) {
       console.log(response.status);
+        if(response.status == 409){
+            $('#username').css('border','solid red 1px');
+        }
     });
   }
 });
